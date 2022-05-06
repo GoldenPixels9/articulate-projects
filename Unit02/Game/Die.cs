7fe442566e7107215e6cf9e7ec4e36d040c9dea8
Thomas Moore <goldenpixels9@gmail.com>
2022-05-06 11:32:59 -0600

    Unit02 successfully completed.

--- Unit02/Game/Die.cs
+++ Unit02/Game/Die.cs
@@ -1,8 +1,31 @@
 using System;
-
+using System.Collections.Generic;
 
 namespace Unit02.Game
 {
+    public class Die {
+
+        public int points = 0;
+        public int value = 0;
+        public Die() {
+        }
+
+        public void Roll() {
+            Random random_roll = new Random();
+            value = random_roll.Next(1, 7);
+            
+             if (value == 5){
+                points = 50;
+            } else if (value == 1) {
+                points = 100;
+            } else if (value == 6){
+                points = 120;
+            } else {
+                points = 0;
+            }
+        }
+    }
+}
     // TODO: Implement the Die class as follows...
     // 1) Add the class declaration. Use the following class comment.
 
@@ -28,4 +51,3 @@ namespace Unit02.Game
         /// worth 50 points, ones are worth 100 points, everything else is worth 0 points.
         /// </summary>
         
-}
